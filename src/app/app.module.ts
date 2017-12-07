db375ade6424ffffa6944f66daeb6af457f2c54e
point-ari-heinonen <ari.heinonen@stud.pointcollege.fi>
2017-12-07 22:14:41 +0200

    Uusi commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
 
+import {ProductListComponent} from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/classes.ts
@@ -0,0 +1,19 @@
+export class ProductGroup {
+    id: number;
+    name: string;
+  }
+  
+export class Product {
+    ProductId: number;
+    Nmae: string;
+    Description: string;
+    Quantity: number;
+    UnitOfMeasure: string;
+    GroupId: number;
+    Entery_Date: Date;
+    Expire: Date;
+    Person_Name: string;
+    Recieved_ammount: number;
+}
+  
+  
